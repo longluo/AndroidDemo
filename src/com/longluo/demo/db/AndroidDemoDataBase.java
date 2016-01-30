a0a0321f11edb4af4841f115e5c9fb9e358296e2
longluo <calmbody@163.com>
2016-01-30 15:07:04 +0800

    upload the Launcher Demo and Content Provider Demo, but without the details.

---
+++ src/com/longluo/demo/db/AndroidDemoDataBase.java
@@ -0,0 +1,5 @@
+package com.longluo.demo.db;
+
+public class AndroidDemoDataBase {
+
+}
---
+++ src/com/longluo/demo/launcher/LauncherActivity.java
@@ -0,0 +1,27 @@
+package com.longluo.demo.launcher;
+
+import com.longluo.demo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher);
+		
+		initViews();
+		initData();
+	}
+	
+	private void initViews() {
+		
+	}
+	
+	private void initData() {
+		
+	}
+
+}
