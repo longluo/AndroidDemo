77ddfc03219de3271c360a3707dd1556b4e7ebbd
luolong <luolong@yulong.com>
2016-05-06 17:23:49 +0800

    The Rounded Image View

--- app/src/main/java/com/longluo/demo/roundedimageview/RoundedImageViewActivity.java
+++ app/src/main/java/com/longluo/demo/roundedimageview/RoundedImageViewActivity.java
@@ -1,14 +1,16 @@
 package com.longluo.demo.roundedimageview;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.longluo.demo.R;
 import com.longluo.demo.widget.image.RoundedImageView;
 
-public class RoundedImageViewActivity extends Activity {
+public class RoundedImageViewActivity extends AppCompatActivity {
     private static final String TAG = RoundedImageViewActivity.class.getSimpleName();
 
+    private Toolbar mToolbar;
     private RoundedImageView mAvatarRIV;
 
     @Override
@@ -20,14 +22,19 @@ public class RoundedImageViewActivity extends Activity {
         initDatas();
     }
 
-    private void initDatas() {
-        mAvatarRIV.setImageResource(R.drawable.meinv01);
-    }
-
     private void initViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.rounded_image_view_demo_activity);
+        setSupportActionBar(mToolbar);
+
         mAvatarRIV = (RoundedImageView) findViewById(R.id.ri_avatar);
 
+    }
+
+    private void initDatas() {
+        mAvatarRIV.setImageResource(R.drawable.meinv01);
 
     }
+
 
 }
