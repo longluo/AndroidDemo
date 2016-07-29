c26316cb4d94d1a2464bc7534ecffe0e1131f2c4
luolong <luolong@yulong.com>
2016-07-29 18:16:09 +0800

    The Shake

--- app/src/main/java/com/longluo/demo/shake/ShakeListener.java
+++ app/src/main/java/com/longluo/demo/shake/ShakeListener.java
@@ -12,27 +12,34 @@ import android.hardware.SensorManager;
 public class ShakeListener implements SensorEventListener {
     // 速度阈值，当摇晃速度达到这值后产生作用
     private static final int SPEED_SHRESHOLD = 2000;
+
     // 两次检测的时间间隔
     private static final int UPTATE_INTERVAL_TIME = 70;
+
     // 传感器管理器
     private SensorManager sensorManager;
+
     // 传感器
     private Sensor sensor;
+
     // 重力感应监听器
     private OnShakeListener onShakeListener;
+
     // 上下文
     private Context mContext;
+
     // 手机上一个位置时重力感应坐标
     private float lastX;
     private float lastY;
     private float lastZ;
+
     // 上次检测时间
     private long lastUpdateTime;
 
     // 构造器
-    public ShakeListener(Context c) {
+    public ShakeListener(Context ctx) {
         // 获得监听对象
-        mContext = c;
+        mContext = ctx;
         start();
     }
 
@@ -41,10 +48,12 @@ public class ShakeListener implements SensorEventListener {
         // 获得传感器管理器
         sensorManager = (SensorManager) mContext
                 .getSystemService(Context.SENSOR_SERVICE);
+
         if (sensorManager != null) {
             // 获得重力传感器
             sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         }
+
         // 注册
         if (sensor != null) {
             sensorManager.registerListener(this, sensor,
@@ -67,11 +76,15 @@ public class ShakeListener implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         // 现在检测时间
         long currentUpdateTime = System.currentTimeMillis();
+
         // 两次检测的时间间隔
         long timeInterval = currentUpdateTime - lastUpdateTime;
+
         // 判断是否达到了检测时间间隔
-        if (timeInterval < UPTATE_INTERVAL_TIME)
+        if (timeInterval < UPTATE_INTERVAL_TIME) {
             return;
+        }
+
         // 现在的时间变成last时间
         lastUpdateTime = currentUpdateTime;
 
@@ -93,6 +106,7 @@ public class ShakeListener implements SensorEventListener {
         double speed = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ
                 * deltaZ)
                 / timeInterval * 10000;
+
         // 达到速度阀值，发出提示
         if (speed >= SPEED_SHRESHOLD) {
             onShakeListener.onShake();
