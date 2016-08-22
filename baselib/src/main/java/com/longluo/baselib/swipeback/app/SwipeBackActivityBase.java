9dc56b9dcde9d134d091376675c52bab85593642
luolong <luolong@yulong.com>
2016-08-22 11:12:16 +0800

    Add the module uilib and baselib.

---
+++ baselib/src/main/java/com/longluo/baselib/swipeback/app/SwipeBackActivityBase.java
@@ -0,0 +1,22 @@
+package com.longluo.baselib.swipeback.app;
+
+
+import com.longluo.baselib.swipeback.SwipeBackLayout;
+
+/**
+ * @author Yrom
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+
+}
